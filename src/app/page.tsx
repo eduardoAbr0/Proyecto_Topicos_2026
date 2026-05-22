37847838641bec2f9f87175b399f5fb9bd6b0d69
ED A <eduardo.br@tecjerez.edu.mx>
2026-05-22 10:55:50 -0600

    Vista para landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,20 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image'
 
-export default function Home() {
+export default function LandingPage() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <section id="landingP" className="row position-relative" style={{ height: '80vh' }}>
+      <Image id='imageL' src="/assets/landindImg.png" alt="Pleasantville landing image" fill style={{ objectFit: 'cover' }} />
+      <div className="overlay">
+        <div className="container text-start">
+          <div className="row">
+            <h4>Pleasantville Community Theater</h4>
+            <p className="d-none d-lg-block">
+              El teatro comunitario de Pleasantville brinda a nuestra entidad la
+              posibilidad de ver el talento de nuestros ciudadanos.
+            </p>
+          </div>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </div>
+    </section>
   );
 }
